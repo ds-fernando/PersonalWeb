54d478e9b37d64f723a08b50d6272f7de6335784
Fernando <fernandoleonsierra@gmail.com>
2024-07-11 23:58:44 -0500

    configuracion

--- app/app.py
+++ app/app.py
@@ -8,6 +8,23 @@ app = Flask(__name__)
 def principal():
     return render_template('index.html')
 
+@app.route('/blog')
+def blog():
+    posts = []
+    posts_dir = 'posts'
+    for filename in os.listdir(posts_dir):
+        if filename.endswith('.md'):
+            with open(os.path.join(posts_dir, filename), 'r', encoding='utf-8') as file:
+                content = file.read()
+                html_content = markdown.markdown(content)
+                posts.append({
+                    'title': filename[:-3].replace('_', ' ').title(),  # Usa el nombre del archivo como título
+                    'content': Markup(html_content)
+                })
+    return render_template('blog.html', posts=posts)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
