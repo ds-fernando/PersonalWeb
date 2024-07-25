711c24fbd40ad3198ef17a1e111042bd626ead9e
Fernando <fernandoleonsierra@gmail.com>
2024-07-24 22:03:41 -0500

    vista blog

--- app/app.py
+++ app/app.py
@@ -16,17 +16,19 @@ def blog():
     posts_dir = 'posts'
     for filename in os.listdir(posts_dir):
         if filename.endswith('.md'):
-            with open(os.path.join(posts_dir, filename), 'r', encoding='utf-8') as file:
-                content = file.read()
-                html_content = markdown2.markdown(content)  # Cambiado a markdown2.markdown
-                posts.append({
-                    'title': filename[:-3].replace('_', ' ').title(),  # Usa el nombre del archivo como título
-                    'content': (html_content)
-                })
+            title = filename[:-3].replace('_', ' ').title()
+            posts.append({'title': title, 'filename': filename}) 
     return render_template('blog.html', posts=posts)
 
 
-
+@app.route('/blog/<filename>')
+def post(filename):
+    posts_dir = 'posts'
+    filepath = os.path.join(posts_dir, filename)
+    with open(filepath, 'r', encoding='utf-8') as file:
+        content = file.read()
+        html_content = markdown2.markdown(content)
+    return render_template('post.html', title=filename[:-3].replace('_', ' ').title(), content=html_content)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
